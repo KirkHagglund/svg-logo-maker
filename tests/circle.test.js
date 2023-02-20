07f9a91682b9b657b9449eb515c045dec132b9bf
elkirko <kirkhagglund@gmail.com>
2023-02-20 09:05:36 -0600

    Updated tests

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -6,7 +6,8 @@ describe('Circle', () => {
     describe('renderShape function', () => {
         it('Should render a shape dynamically with color', () => {
             const shape = new Circle();
-            expect(shape.shapeRender()).toEqual(`<circle cx="150" cy="110" r="80" fill="blue"/>`);
+            shape.renderShape('blue');
+            expect(shape.renderShape()).toEqual(`<circle cx="150" cy="110" r="80" fill="${color}"/>`);
         });
     });
 });
